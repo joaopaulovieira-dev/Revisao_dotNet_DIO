d71b16ba823a783a6aeb211326c628424a2bdfb2
João Paulo Vieira <joaopaulovieira.dev@gmail.com>
2021-09-02 15:58:11 -0300

    First Aplication

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Revisao_dotNet_DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a linha desejada: ");
+            Console.WriteLine("1- Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular media geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //TODO: adicionar aluno
+                        break;
+                    case "2":
+                        //TODO: listar alunos
+                        break;
+                    case "3":
+                        //TODO: calcular media geral
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+    }
+}
